31ba55617e605ceed08b3dd67f75b506dfaef463
ahtrahdis7 <sidharthabiki@gmail.com>
2021-07-17 10:49:35 +0530

    Updates

---
+++ precache-manifest.895a63e60d2f7ad6f18911006b8f61c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec313ab4d113ad69eba73f68aacd7d1",
+    "url": "/ogb/index.html"
+  },
+  {
+    "revision": "0ee59046c41a123b8985",
+    "url": "/ogb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "436df9dd89478a9ce856",
+    "url": "/ogb/static/js/2.8d211156.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ogb/static/js/2.8d211156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee59046c41a123b8985",
+    "url": "/ogb/static/js/main.42e4769a.chunk.js"
+  },
+  {
+    "revision": "450c1021e75051bcf593",
+    "url": "/ogb/static/js/runtime-main.6d0cb8a4.js"
+  },
+  {
+    "revision": "60f20e7fea06e6a3605f7b489e0521c8",
+    "url": "/ogb/static/media/1.60f20e7f.png"
+  },
+  {
+    "revision": "826758485e4eb99616d868132ac1135c",
+    "url": "/ogb/static/media/2.82675848.png"
+  },
+  {
+    "revision": "ceba72eba82e6d86eb28154ca58e2054",
+    "url": "/ogb/static/media/3.ceba72eb.png"
+  },
+  {
+    "revision": "c686c22d717a7b479504bffeb96811a0",
+    "url": "/ogb/static/media/tagline.c686c22d.png"
+  }
+]);
